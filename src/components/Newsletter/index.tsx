cf94e5d4defcb5983d97cde4cd40a6f2dff0c228
hosnipogi <hosnibona@gmail.com>
2022-04-03 02:16:45 +0800

    loading icon

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 // import debounce from 'lodash/debounce'
 import axios, { AxiosRequestConfig } from 'axios'
 import { string } from 'yup'
+import Loading from '../Loader/CircleLoader'
 
 const MainWrapper = styled(Flex)`
   flex-direction: column;
@@ -26,11 +27,19 @@ const StyledInput = styled(Input)`
   margin-right: 10px;
   background-color: ${dark.colors.input};
   border-color: ${dark.colors.inputSecondary};
+  color: ${dark.colors.text};
+  max-width: 244px;
 `
 
 const StyledButton = styled(Button)`
   height: 40px;
 `
+
+const Loader = () => (
+  <Flex minWidth="125px" height="40px" justifyContent="center" alignItems="center">
+    <Loading style={{ width: '22px', height: 'auto' }} />
+  </Flex>
+)
 
 const emailSchema = string().email()
 const url = 'https://api.fortcake.io/subscribe'
@@ -78,20 +87,22 @@ const SearchInput: React.FC = () => {
     <MainWrapper>
       <WrapWrapper>
         <Text mb="24px" lineHeight="1" color={dark.colors.text}>
-          Receive the latest games and news in your inbox
+          Receive the latest releases & giveaways in your inbox
         </Text>
         {!success ? (
           <form>
             <InputWrapper>
               <StyledInput value={email} onChange={onChange} placeholder="Enter your email address" />
-              {isLoading && <h1>WAIT</h1>}
-              <StyledButton variant="primary" type="submit" onClick={handleSubmit}>
-                Subscribe
-              </StyledButton>
+              {!isLoading && (
+                <StyledButton variant="primary" type="submit" onClick={handleSubmit}>
+                  Subscribe
+                </StyledButton>
+              )}
+              {isLoading && <Loader />}
             </InputWrapper>
           </form>
         ) : (
-          <Text>Talk to you soon!</Text>
+          <Text color={dark.colors.text}>Thank you for subscribing!</Text>
         )}
       </WrapWrapper>
     </MainWrapper>
