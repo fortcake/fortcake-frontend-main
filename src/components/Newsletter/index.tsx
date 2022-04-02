a08a997343c8f414cf86dc5c1a8a3c62c9928328
hosnipogi <hosnibona@gmail.com>
2022-04-02 17:05:29 +0800

    fix: Cors

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import { Input, Button, Flex, Text, dark } from 'fortcake-uikit-v2'
 import styled from 'styled-components'
 // import debounce from 'lodash/debounce'
-import { useTranslation } from 'contexts/Localization'
 import axios, { AxiosRequestConfig } from 'axios'
+import { string } from 'yup'
 
 const MainWrapper = styled(Flex)`
   flex-direction: column;
@@ -32,30 +32,24 @@ const StyledButton = styled(Button)`
   height: 40px;
 `
 
-const url = 'https://api.fortcake.com/subscribe'
-
-const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    )
-}
+const emailSchema = string().email()
+const url = 'https://api.fortcake.io/subscribe'
 
 const SearchInput: React.FC = () => {
   const [email, setEmail] = useState('')
   const [success, setSuccess] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value)
   }
 
-  const handleSubmit = async (event: React.MouseEvent<HTMLElement>) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     try {
-      event.preventDefault()
       if (!email) throw new Error('Nothing here')
-      const validatedEmail = validateEmail(email)
-      if (!validatedEmail) throw new Error('Invalid email')
+      const eadd = await emailSchema.validate(email)
+      setIsLoading(true)
 
       const options: AxiosRequestConfig = {
         method: 'POST',
@@ -65,15 +59,18 @@ const SearchInput: React.FC = () => {
           'Content-Type': 'application/json',
         },
         data: {
-          email,
+          email: eadd,
         },
       }
 
       const { data } = await axios.request(options)
-      console.log({ data })
-      setSuccess(!success)
-    } catch (e) {
-      console.log({ e })
+      if (data[0].email === email) {
+        setIsLoading(false)
+        setSuccess(true)
+      }
+    } catch (error) {
+      setIsLoading(false)
+      console.info({ error })
     }
   }
 
@@ -84,12 +81,15 @@ const SearchInput: React.FC = () => {
           Receive the latest games and news in your inbox
         </Text>
         {!success ? (
-          <InputWrapper>
-            <StyledInput value={email} onChange={onChange} placeholder="Enter your email address" />
-            <StyledButton variant="primary" onClick={handleSubmit}>
-              Subscribe
-            </StyledButton>
-          </InputWrapper>
+          <form>
+            <InputWrapper>
+              <StyledInput value={email} onChange={onChange} placeholder="Enter your email address" />
+              {isLoading && <h1>WAIT</h1>}
+              <StyledButton variant="primary" type="submit" onClick={handleSubmit}>
+                Subscribe
+              </StyledButton>
+            </InputWrapper>
+          </form>
         ) : (
           <Text>Talk to you soon!</Text>
         )}
